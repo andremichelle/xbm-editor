3ffcd633ec2215e7df0e827eb2a27321fc5842be
André Michelle <andre.michelle@gmail.com>
2022-09-20 06:15:22 +0200

    create new sprite

--- typescript/xbm-editor/view.ts
+++ typescript/xbm-editor/view.ts
@@ -8,11 +8,29 @@ export class Env {
 
 export class SheetView {
     readonly element: HTMLElement = HTML.create('div', { class: 'sheet-view' })
+    readonly spriteContainer: HTMLDivElement = HTML.create('div', { class: 'sprite-container' })
+    readonly addSpriteButton = HTML.create('button', { textContent: '+' })
     readonly spriteViews: SpriteView[]
 
     constructor(readonly env: Env, readonly sheet: xbm.Sheet) {
         this.spriteViews = sheet.sprites.map(sprite => new SpriteView(env, sprite))
-        this.spriteViews.forEach(view => this.element.appendChild(view.element))
+        this.spriteViews.forEach(view => this.spriteContainer.appendChild(view.element))
+        this.element.appendChild(this.spriteContainer)
+        this.element.appendChild(this.addSpriteButton)
+
+        this.addSpriteButton.addEventListener('click', () => {
+            const sizeInput = prompt('Enter size (w x h)', '8x8')
+            if (sizeInput === null) return
+            const sizeArray = sizeInput.split('x').map(x => parseInt(x.trim()))
+            if (sizeArray.length !== 2) return
+            if (sizeArray.some(x => isNaN(x))) return
+            const name = prompt('Enter name', 'untitled')
+            if (name === null || name.length === 0) return
+            const spriteView = new SpriteView(this.env, xbm.Sprite.single(sizeArray[0], sizeArray[1], name))
+            this.spriteViews.push(spriteView)
+            this.spriteContainer.appendChild(spriteView.element)
+            console.log(`new name: ${name}, w: ${sizeArray[0]}, h: ${sizeArray[1]}`)
+        })
     }
 }
 
@@ -22,16 +40,15 @@ export class SpriteView {
     readonly frameViews: FrameView[]
 
     constructor(readonly env: Env, readonly sprite: xbm.Sprite) {
-        this.element.append(HTML.create('h1', { textContent: sprite.getName() }))
-        this.element.appendChild(this.frameContainer)
         this.frameViews = this.sprite.getFrames().map(frame => new FrameView(env, frame))
         this.frameViews.forEach(view => this.frameContainer.appendChild(view.element))
+        this.element.append(HTML.create('h1', { textContent: sprite.getName() }))
+        this.element.appendChild(this.frameContainer)
     }
 }
 
 export class FrameView {
     readonly element: HTMLElement = HTML.create('div', { class: 'frame-view' })
-    readonly footer: HTMLElement = HTML.create('footer')
     readonly canvas: HTMLCanvasElement = document.createElement('canvas')
     readonly context: CanvasRenderingContext2D = this.canvas.getContext('2d')!
 
@@ -45,7 +62,6 @@ export class FrameView {
             this.update()
         })
         this.element.appendChild(this.canvas)
-        this.element.appendChild(this.footer)
         this.update()
     }
 
@@ -53,6 +69,7 @@ export class FrameView {
         const z = this.env.zoom.get()
         this.canvas.width = this.frame.getWidth() * z
         this.canvas.height = this.frame.getHeight() * z
+        this.context.fillStyle = 'white'
         for (let y = 0; y < this.frame.getHeight(); ++y) {
             for (let x = 0; x < this.frame.getWidth(); ++x) {
                 if (this.frame.getPixel(x, y)) {
